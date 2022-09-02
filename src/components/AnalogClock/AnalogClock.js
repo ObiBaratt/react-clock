53900260a11abee7af522818ff92136f3731902d
Obi Baratt <obibaratt@gmail.com>
2022-09-02 15:29:58 -0700

    Add AnalogClock logic

--- src/components/AnalogClock/AnalogClock.js
+++ src/components/AnalogClock/AnalogClock.js
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react";
 import "./AnalogClock.css";
+// import "../../styles/general.css";
 
 export default function AnalogClock() {
+    const [date, setDate] = useState(new Date());
+    const [hour, setHour] = useState(date.getHours());
+    const [min, setMin] = useState(date.getMinutes());
+    const [sec, setSec] = useState(date.getSeconds());
+
+    const updateTime = () => {
+        setDate(new Date());
+        console.log(date);
+        setHour(date.getHours());
+        setMin(date.getMinutes());
+        setSec(date.getSeconds());
+    };
+
+
+    useEffect(() => {
+        const secondTimer = setInterval(updateTime, 1000);
+        return () => {
+            clearInterval(secondTimer);
+        };
+    });
+
     return (
         <div className="clockBody">
             <div className="analogClock">
@@ -10,9 +33,21 @@ export default function AnalogClock() {
                     <div className="mark onetwenty"></div>
                     <div className="mark onefifty"></div>
                     <div className="innerClock">
-                        <div className="clockHand hour"></div>
-                        <div className="clockHand min"></div>
-                        <div className="clockHand sec"></div>
+                        <div className="clockHand hour"
+                            style={{
+                                transform: `rotate(${hour * 30 + 90}deg)`
+                            }}>
+                        </div>
+                        <div className="clockHand min"
+                            style={{
+                                transform: `rotate(${min * 6 + 90}deg)`
+                            }}>
+                        </div>
+                        <div className="clockHand sec"
+                        style={{
+                            transform: `rotate(${sec * 6 + 90}deg)`
+                        }}>
+                        </div>
                     </div>
                 </div>
             </div>
