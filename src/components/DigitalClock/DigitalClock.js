fe17720c208c22e9b568f34e8f1d52edfa34ccf5
Obi Baratt <obibaratt@gmail.com>
2022-09-02 15:29:25 -0700

    DigitalClock cleanup

--- src/components/DigitalClock/DigitalClock.js
+++ src/components/DigitalClock/DigitalClock.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import "./DigitalClock.css";
-import "../../styles/general.css"
+import "../../styles/general.css";
 
 export default function DigitalClock() {
-    const [date, setDate] = useState(new Date())
+    const [date, setDate] = useState(new Date());
 
     const updateTime = () => {
         setDate(new Date());
-    }
+    };
 
     useEffect(()=> {
         const secondTimer = setInterval(updateTime, 1000);
